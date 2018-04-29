9bd35cb2c02c2361d33b73387c15a9e8ff2eada7
Crash122 <38176346+Crash122@users.noreply.github.com>
2018-04-29 14:17:16 +0500

    Add files via upload

---
+++ AuthorizationForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xNet;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace WindowsFormsApp14
+{
+    public partial class AuthorizationForm : Form
+    {
+        public AuthorizationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthorizationForm_Load(object sender, EventArgs e)
+        {
+            GetToken.DocumentCompleted += GetToken_DocumentCompleted;
+            GetToken.Navigate("https://oauth.vk.com/authorize?client_id=5709976&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52");
+        }
+
+        private void GetToken_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (GetToken.Url.ToString().IndexOf("access_token=") != 0)
+            {
+                GetUserToken();
+            }
+        }
+        private void GetUserToken()
+        {
+            char[] Symbols = { '=', '&' };
+            string[] URL = GetToken.Url.ToString().Split(Symbols);
+            File.WriteAllText("UserInf.txt", URL[1] + "\n");
+            File.AppendAllText("UserInf.txt", URL[5]);
+            this.Visible = false;
+        }
+    }
+}
